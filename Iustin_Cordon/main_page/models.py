30aec486a96a1981e4b3f5ba53fee6d4bc5899fa
JustinMNIC <104854769+JustinMNIC@users.noreply.github.com>
2024-03-20 23:55:55 +0200

    20/03/2024

--- Iustin_Cordon/main_page/models.py
+++ Iustin_Cordon/main_page/models.py
@@ -7,7 +7,6 @@ class Project(models.Model):
     description = models.TextField(blank=True)
     link_to_github = models.URLField(blank=True)
     link_see_in_action = models.URLField(blank=True)
-    image_of_video = models.FileField(upload_to='media/', blank=True)
     tags = models.ManyToManyField('Tag')
     
     def __str__(self):
@@ -15,7 +14,6 @@ class Project(models.Model):
     
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique = True)
-    projects = models.ManyToManyField(Project, blank=True)
     
     def __str__(self):
         return self.name
--- Iustin_Cordon/main_page/views.py
+++ Iustin_Cordon/main_page/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def get_projects_count_for_tag(tag_name):
+    return Project.objects.filter(tags__name=tag_name).count()
+
 def main_index(request):
-    return render(request, 'main_page/index.html')
+    websites_projects_count = get_projects_count_for_tag('Website')
+    app_projects_count = get_projects_count_for_tag('App')
+    local_projects_count = get_projects_count_for_tag('Local')
+    games_projects_count = get_projects_count_for_tag('Game')
+    bots_projects_count = get_projects_count_for_tag('Bot')
+    
+    projects = Project.objects.all()
+        
+    return render(request, 'main_page/index.html', {
+        'websites_projects_count': websites_projects_count,
+        'app_projects_count': app_projects_count,
+        'local_projects_count': local_projects_count,
+        'games_projects_count': games_projects_count,
+        'bots_projects_count': bots_projects_count,
+        'projects': projects
+    })
